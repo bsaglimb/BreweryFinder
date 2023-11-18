49c0ce6feea9df6fe06c375993183ec0d4ae929b
exsider11 <exsider1231@gmail.com>
2023-11-18 02:56:54 -0500

    added weather info

--- assets/js/results.js
+++ assets/js/results.js
@@ -17,7 +17,7 @@ var titleID = document.getElementById("breweryTitle")
 var contentID = document.getElementById("breweryContent")
 
 async function getWeatherData(latitude, longitude) {
-    const urlCoordinates = `http://www.7timer.info/bin/api.pl?lon=${longitude}&lat=${latitude}&product=astro&output=json`;
+    const urlCoordinates = `http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=astro&output=json`;
 
     return fetch(urlCoordinates)
         .then(response => response.json());
@@ -35,6 +35,78 @@ async function getBreweryByCity(city) {
 
     return fetch(urlCity)
         .then(response => response.json());
+}
+
+function setWebpage(city){
+    timer1.innerText = time1;
+    temp1.innerText = "Temperature: "+Math.round(dayTemp1);
+    wind1.innerText = "Wind: "+ dayWind1;
+    humid1.innerText = "Humid: " + dayHumid1;
+    timer2.innerText = time2;
+    temp2.innerText = "Temperature: "+Math.round(dayTemp2);
+    wind2.innerText = "Wind: "+ dayWind2;
+    humid2.innerText = "Humid: " + dayHumid2;
+    timer3.innerText = time3;
+    temp3.innerText = "Temperature: "+Math.round(dayTemp3);
+    wind3.innerText = "Wind: "+ dayWind3;
+    humid3.innerText = "Humid: " + dayHumid3;
+    timer4.innerText = time4;
+    temp4.innerText = "Temperature: "+Math.round(dayTemp4);
+    wind4.innerText = "Wind: "+ dayWind4;
+    humid4.innerText = "Humid: " + dayHumid4;
+    timer5.innerText = time5;
+    temp5.innerText = "Temperature: "+Math.round(dayTemp5);
+    wind5.innerText = "Wind: "+ dayWind5;
+    humid5.innerText = "Humid: " + dayHumid5;
+}
+
+function getApiWeather(lat,long){
+    var requestURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+long+'&appid=886f3f4f8785854297f7a562176e6a41';
+    fetch(requestURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data)
+        dayTemp1 = (data.list[7].main.temp - 273.15) * 9/5 + 32;
+        dayTemp2 = (data.list[15].main.temp - 273.15) * 9/5 + 32;
+        dayTemp3 = (data.list[23].main.temp - 273.15) * 9/5 + 32;
+        dayTemp4 = (data.list[31].main.temp - 273.15) * 9/5 + 32;
+        dayTemp5 = (data.list[39].main.temp - 273.15) * 9/5 + 32;
+
+        dayWind1 = data.list[7].wind.speed + " MPH";
+        dayWind2 = data.list[15].wind.speed + " MPH";
+        dayWind3 = data.list[23].wind.speed + " MPH";
+        dayWind4 = data.list[31].wind.speed + " MPH";
+        dayWind5 = data.list[39].wind.speed + " MPH";
+
+        dayHumid1 = data.list[7].main.humidity + "%";
+        dayHumid2 = data.list[15].main.humidity + "%";
+        dayHumid3 = data.list[23].main.humidity + "%";
+        dayHumid4 = data.list[31].main.humidity + "%";
+        dayHumid5 = data.list[39].main.humidity + "%";
+
+        time1 = data.list[7].dt_txt;
+        time2 = data.list[15].dt_txt;
+        time3 = data.list[23].dt_txt;
+        time4 = data.list[31].dt_txt;
+        time5 = data.list[39].dt_txt;
+
+        time = data.list[3].dt_txt;
+        setWebpage(data.city.name);
+    })
+}
+async function getApiLocation(city){
+    var requestURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=1&appid=886f3f4f8785854297f7a562176e6a41'
+    fetch(requestURL)
+    .then(function(reponse){
+        return reponse.json();
+    })
+    .then(function(data){
+        var latitude = data[0].lat;
+        var longitude = data[0].lon;
+        getApiWeather(latitude,longitude)
+    })
 }
 
 async function displayBreweryResults(city) {
@@ -80,6 +152,14 @@ async function displayBreweryResults(city) {
                         contentID.innerText = "Address: " + brewery.address_1 + "\n" + "Website: No website URL for this Brewery!";
                     }
 
+                    if(brewery.latitude,brewery.longitude){ 
+                        getApiWeather(brewery.latitude,brewery.longitude);
+                        console.log("this has lonlat")
+                    }
+                    else{
+                        getApiLocation(city);
+                        console.log("this does not have lonlat")
+                    }     
                 })
 
             } else {
